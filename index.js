1aa50cdc83e3680879589c20bcc53bde8b64a4a5
Alexander <yakubov.2003@mail.ru>
2023-05-21 18:57:09 +0200

    EsLint

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('Hello world!');
+
+
+import express from 'express';
+var app = express();
+const port = 4000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+const a = 500;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
